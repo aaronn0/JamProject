c2284319567a711a152ca278eb6fc40fe1a7bcb5
aaronn0 <aaronnoh5@gmail.com>
2025-10-30 01:20:37 -0400

    Basic Outline

--- Player/player.gd
+++ Player/player.gd
@@ -4,6 +4,10 @@ extends CharacterBody3D
 const SPEED = 5.0
 
 
+
+func _ready() -> void:
+	axis_lock_angular_y = true
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -11,7 +15,7 @@ func _physics_process(delta: float) -> void:
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -19,5 +23,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		
 	move_and_slide()
